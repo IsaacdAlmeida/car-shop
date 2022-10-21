f5656e0e74a85b383438990d1dee631486ef8c21
Isaac Almeida <isaacalmeidafilho@gmail.com>
2022-10-21 15:13:02 -0300

    feat: create interface ICar

---
+++ src/interfaces/ICar.ts
@@ -0,0 +1,22 @@
+import { z } from 'zod';
+import { VehicleZodSchema } from './IVehicle';
+
+const CarZodSchema = VehicleZodSchema.extend({
+  doorsQty: z.number({
+    required_error: 'Year is required',
+    invalid_type_error: 'Year must be a number',
+  }).int().positive()
+    .gte(2, { message: 'Doors must be greater or equal than 2' })
+    .lte(4, { message: 'Doors must be lesser or equal than 4' }),
+
+  seatsQty: z.number({
+    required_error: 'Seats is required',
+    invalid_type_error: 'Seats must be a number',
+  }).int().positive()
+    .gte(2, { message: 'Seats must be greater or equal than 2' })
+    .lte(7, { message: 'Seats must be lesser or equal than 7' }),
+});
+
+export type ICar = z.infer<typeof CarZodSchema>;
+
+// https://stackoverflow.com/questions/72867815/wrap-up-zod-validations-for-reuse
