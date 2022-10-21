f8ae883ce30e71a434972faffd6149c7b7f853e4
Isaac Almeida <isaacalmeidafilho@gmail.com>
2022-10-21 14:42:58 -0300

    feat: creat interface model and add a index, now the app can run on port 3001

---
+++ src/interfaces/IModel.ts
@@ -0,0 +1,7 @@
+export interface IModel<T> { 
+  create(obj:T):Promise<T>, 
+  read():Promise<T[]>
+  readOne(_id:string):Promise<T | null>,
+  update(_id:string, obj:Partial<T>):Promise<T | null>
+  delete(_id:string):Promise<T | null>
+}
