624c08b0e80573b0a890bd4d4510e8b7909f7785
Isaac Almeida <isaacalmeidafilho@gmail.com>
2022-10-21 18:13:57 -0300

    feat: create interface IMotorcycle

---
+++ src/interfaces/IMotorcycle.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod';
+import { VehicleZodSchema } from './IVehicle';
+
+export const MotorcycleZodSchema = VehicleZodSchema.extend({
+  category: z.enum(['Street', 'Custom', 'Trail']),
+
+  engineCapacity: z.number({
+    required_error: 'Engine Capacity is required',
+    invalid_type_error: 'Engine Capacity must be a number',
+  }).int().positive()
+    .lte(2500, { message: 'ngine Capacity must be lesser or equal than 1900' })
+});
+
+export type IMotorcycle = z.infer<typeof MotorcycleZodSchema>;
